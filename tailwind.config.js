dcf8713fc91695b8038799a2d2565d5a2590230d
kiseon77 <kieson.han77@gmail.com>
2024-08-07 13:08:41 +0900

    tailwind chage

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        yellow: "#d7fa00",
+        gray: "rgb(160, 160, 160)",
+      },
+    },
+  },
+  plugins: [],
+};
